a3c1914b62ab816b2631053716d9733e1f9f914b
247399462@qq.com <247399462@qq.com>
2019-06-25 09:21:59 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//注册页面
+Route::get('/signup','UsersController@create')->name('signup');
