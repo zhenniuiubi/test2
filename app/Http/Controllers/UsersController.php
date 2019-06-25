5d9b5eeaa093a5bbef43816add7ef847e570bea9
247399462@qq.com <247399462@qq.com>
2019-06-25 11:47:14 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,9 +10,19 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
-
+    //显示注册页面
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+    //注册逻辑
+    public function store()
+    {
+        //验证 name email password
+        $this->validate(request(),[
+            'name' => 'required|min:5|max:10',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confired',
+        ]);
     }
 }
