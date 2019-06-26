114a6e060779099213654b875dab7a57d139cfe1
247399462@qq.com <247399462@qq.com>
2019-06-26 11:41:01 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show','create','store'],
+            'except' => ['show','create','store','index'],
         ]);
 
         $this->middleware('guest',[
@@ -24,7 +24,8 @@ class UsersController extends Controller
 
     public function index()
     {
-        return view('users.create');
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
     }
 
     //显示注册页面
